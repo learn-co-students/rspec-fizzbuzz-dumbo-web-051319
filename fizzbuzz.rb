2c75cd31a7bcf91d1ea10934db8b3a618aec688b
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-01 18:24:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,13 @@
 
 def fizzbuzz(number)
   
-  if number % 3 == 0 
-   return "Fizz"
+  if number % 3 == 0 && number % 5 == 0 
+   return "FizzBuzz"
   
   elsif number % 5 == 0
    return "Buzz"
   
-  elsif number % 3 == 0 && number % 5 == 0 
+  elsif number % 3 == 0 
     return "FizzBuzz" 
   
   else
